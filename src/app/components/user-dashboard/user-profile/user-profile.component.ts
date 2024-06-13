6e69384a01044a10cf58c16cfb22d5efbc000fb1
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-06-13 10:11:21 +0530

    Update user-profile.component.ts

--- src/app/components/user-dashboard/user-profile/user-profile.component.ts
+++ src/app/components/user-dashboard/user-profile/user-profile.component.ts
@@ -22,21 +22,17 @@ export class UserProfileComponent implements OnInit {
     role: ''
 
   };  
-
-  };  // Initialize the employee object
+// Initialize the employee object
 
   employeeId: string;
 
-
   constructor(private employeeService: EmployeeService, private auth: AuthService) {}
-
 
   ngOnInit(): void {
     this.employeeId = this.auth.getEmployeeId();
     this.getDetailsOfEmployee();
   }
 
-  
   private getDetailsOfEmployee() {
     this.employeeService.getEmployeeDetails(this.employeeId).subscribe((data) => {
       console.log(data);
