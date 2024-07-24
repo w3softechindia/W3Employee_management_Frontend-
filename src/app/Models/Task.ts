0542057f223894f2a5f830c9c66a32e4118f7506
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-07-24 09:37:17 +0530

    Aman (#60)
    * 02-07-2024
    
    * 02-07-2024
    
    * 02-07-2024
    
    * 03-07-2023
    
    * 08-07-2024
    
    * 08-07-2024
    
    * 09-07-2024
    
    * 11-07-2024
    
    * 11-07-2024
    
    * 16-07-2024
    
    * 16-07-2024
    
    * 19-07-2024
    
    * 19-07-2024
    
    * 24-07-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>

--- src/app/Models/Task.ts
+++ src/app/Models/Task.ts
@@ -1,10 +1,10 @@
 export class Task {
-    taskId: string;
-    taskName: string;
-    description: string;
-    startDate: string;
-    endDate: string;
-    status: string;
-teamName: any;
-  }
-  
+  taskId: string;
+  taskName: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+  status: string;
+  teamName: any;
+  fileUrl?: string; 
+}
