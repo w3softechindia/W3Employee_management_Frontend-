1cbe84e9e3450e54fec7228dd6a6a9e65e974b32
Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
2024-09-06 17:23:02 +0530

    Nani (#11)
    * 06/09/2024
    
    * Nani-06/09/2024
    
    * 06/09/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -76,8 +76,6 @@ import { InstructorSettingsComponent } from './components/instructor-dashboard/i
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AuthInterceptor } from './auth/interceptor/auth-interceptor.interceptor';
-import { AddTeamComponent } from './components/instructor-dashboard/instructor-add-team/add-team.component';
-
 import { TeamDetailsComponent } from './components/instructor-dashboard/team-details/team-details.component';
 import { AdminEmployeesComponent } from './components/admin-dashboard/admin-employees/admin-employees.component';
 import { TeamleadEmployeesComponent } from './components/admin-dashboard/teamlead-employees/teamlead-employees.component';
@@ -125,8 +123,8 @@ import { TeamleadAttendanceTrackComponent } from './components/instructor-dashbo
 import { RmsInterviewComponent } from './components/rms_component/rms-interview/rms-interview.component';
 import { RmsNavbarComponent } from './components/rms_component/rms-navbar/rms-navbar.component';
 import { BdmNavbarComponent } from './components/bdm_component/bdm-navbar/bdm-navbar.component';
+import { AddTeamComponent } from './components/admin-dashboard/instructor-add-team/add-team.component';
 import { BdmClientComponent } from './components/bdm_component/bdm-client/bdm-client.component';
-
 
 
 
--- src/app/components/bdm_component/bdm-navbar/bdm-navbar.component.ts
+++ src/app/components/bdm_component/bdm-navbar/bdm-navbar.component.ts
@@ -1,13 +1,11 @@
-
 import { Component, OnInit } from '@angular/core';
-import { Component } from '@angular/core';
-
 
 @Component({
   selector: 'app-bdm-navbar',
   templateUrl: './bdm-navbar.component.html',
   styleUrls: ['./bdm-navbar.component.scss']
 })
+
 export class BdmNavbarComponent implements OnInit {
 
   constructor() { }
