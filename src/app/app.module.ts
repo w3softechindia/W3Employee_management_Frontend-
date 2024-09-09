1cc59d849c19a0dd1d8d3a12dbf4172983cb19ed
vinitha111 <161420839+vinitha111@users.noreply.github.com>
2024-09-09 09:58:49 +0530

    Vinitha (#14)
    * 06-09-2024
    
    * 09-09-2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -74,7 +74,9 @@ import { InstructorEarningsComponent } from './components/instructor-dashboard/i
 import { InstructorWithdrawComponent } from './components/instructor-dashboard/instructor-withdraw/instructor-withdraw.component';
 import { InstructorSettingsComponent } from './components/instructor-dashboard/instructor-settings/instructor-settings.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
 import { AuthInterceptor } from './auth/interceptor/auth-interceptor.interceptor';
+
 import { TeamDetailsComponent } from './components/instructor-dashboard/team-details/team-details.component';
 import { AdminEmployeesComponent } from './components/admin-dashboard/admin-employees/admin-employees.component';
 import { TeamleadEmployeesComponent } from './components/admin-dashboard/teamlead-employees/teamlead-employees.component';
@@ -121,10 +123,12 @@ import { LeavesComponentComponent } from './components/admin-dashboard/leaves/le
 import { TeamleadAttendanceTrackComponent } from './components/instructor-dashboard/teamlead-attendance-track/teamlead-attendance-track.component';
 import { RmsInterviewComponent } from './components/rms_component/rms-interview/rms-interview.component';
 import { RmsNavbarComponent } from './components/rms_component/rms-navbar/rms-navbar.component';
+
 import { BdmNavbarComponent } from './components/bdm_component/bdm-navbar/bdm-navbar.component';
 import { BdmClientComponent } from './components/bdm_component/bdm-client/bdm-client.component';
 import { AddTeamComponent } from './components/admin-dashboard/instructor-add-team/add-team.component';
 import { BdmSettingComponent } from './components/bdm_component/bdm-setting/bdm-setting.component';
+
 
 
 
@@ -196,7 +200,6 @@ import { BdmSettingComponent } from './components/bdm_component/bdm-setting/bdm-
         InstructorEarningsComponent,
         InstructorWithdrawComponent,
         InstructorSettingsComponent,
-        AddTeamComponent,
         TeamDetailsComponent,
         AdminEmployeesComponent,
         TeamleadEmployeesComponent,
@@ -215,6 +218,7 @@ import { BdmSettingComponent } from './components/bdm_component/bdm-setting/bdm-
         ChatBotComponent,
         SessionsPageComponent,
         InstructorTeamsComponent,
+        AddTeamComponent,
 
         AdminEventsComponent,
 
