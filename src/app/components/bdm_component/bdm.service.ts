76bb7e04ee3e3fc30d9b22c32546b1dc1df224a9
Sri-Charan-0516 <sricharan824@gmail.com>
2024-09-26 10:48:14 +0530

    26-09-2024

--- src/app/components/bdm_component/bdm.service.ts
+++ src/app/components/bdm_component/bdm.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { catchError, Observable, of } from 'rxjs';
 import { AuthService } from 'src/app/auth/auth.service';
 import { BdmClient } from 'src/app/Models/bdmClient';
-import { Deployment } from 'src/app/Models/Deployment';
+import { Deployment } from 'src/app/Models/deployment';
 import { Employee } from 'src/app/Models/Employee';
 
 @Injectable({
