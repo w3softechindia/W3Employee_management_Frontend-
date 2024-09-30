5d6b0713721718319d82e97cb47924294f3fb5c1
Sachin Putta <33392966+sachinputta@users.noreply.github.com>
2024-09-30 11:03:59 +0530

    Sachin (#77)
    * 10:35 (#15)
    
    * 06-09-2024 (#4)
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06/09/2024 (#5)
    
    * Chidwi (#6)
    
    * 06-09-2024
    
    * 06-09-2024
    
    ---------
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06-09-2024 (#7)
    
    * Nani (#8)
    
    * 06/09/2024
    
    * Nani-06/09/2024
    
    * Chidwi (#9)
    
    * 06-09-2024
    
    * 06-09-2024
    
    * 06-09-2024
    
    ---------
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06-09-2024 (#10)
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Nani (#11)
    
    * 06/09/2024
    
    * Nani-06/09/2024
    
    * 06/09/2024
    
    * Aman (#12)
    
    * 06-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Aman (#13)
    
    * 06-09-2024
    
    * 09-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Vinitha (#14)
    
    * 06-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Gangineni Chinmay Chidwi <126610157+ChinmayChidwi@users.noreply.github.com>
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    Co-authored-by: Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
    Co-authored-by: vinitha111 <161420839+vinitha111@users.noreply.github.com>
    Co-authored-by: Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
    
    * 11-09-2024
    
    * 11-09-2024
    
    * 13-09-2024
    
    * sachin
    
    * 17-09-2024
    
    * 26-09-2024
    
    * 30-09-2024
    
    ---------
    
    Co-authored-by: Gangineni Chinmay Chidwi <126610157+ChinmayChidwi@users.noreply.github.com>
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    Co-authored-by: Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
    Co-authored-by: vinitha111 <161420839+vinitha111@users.noreply.github.com>
    Co-authored-by: Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -515,6 +515,7 @@ const routes: Routes = [
     path: 'bdm-navbar',
     component: BdmNavbarComponent,
     canActivate: [bdmGuard],
+
   },
   { path: 'notfound', component: NotFoundComponent }, // This line will remain down from the whole pages component list
 ];
