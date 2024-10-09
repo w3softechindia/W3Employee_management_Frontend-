e774e147eaa78202fd985fe905de588e6e3dfb1b
ChinmayChidwi <chidwi4@gmail.com>
2024-10-09 15:13:43 +0530

    09-10-2024

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -493,7 +493,7 @@ const routes: Routes = [
   },
   {
 
-    path: 'rms-employees',
+    path: 'employee-interview-details',
     component: EmployeeInterviewDetailsComponent,
     canActivate: [rmsAdminGuard],
   },
