3baaa38d7355a3cfcb78d0ceb77584590d67b69b
Amanmulla6499 <amanmulla6499>
2024-10-14 10:06:54 +0530

    14-10-2024

--- src/app/components/rms_component/rms-service.service.ts
+++ src/app/components/rms_component/rms-service.service.ts
@@ -6,47 +6,44 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RmsServiceService {
-
-
   // private baseurl = 'http://localhost:5050';
-  
-  
-  
-  private baseurl =
-  'https://lms-backend-5e890b1bbe26.herokuapp.com';
 
+  private baseurl = 'https://lms-backend-5e890b1bbe26.herokuapp.com';
 
-  constructor(private http: HttpClient) { }
-
-
- 
+  constructor(private http: HttpClient) {}
 
   getTeamLeads(): Observable<Employee[]> {
     return this.http.get<Employee[]>(`${this.baseurl}/getTeamLeads`);
   }
 
-  scheduleInterview(interview: Rms_Interview, teamLeadId: string): Observable<Rms_Interview> {
-    return this.http.post<Rms_Interview>(`${this.baseurl}/scheduleInterview/${teamLeadId}`, interview);
+  scheduleInterview(
+    interview: Rms_Interview,
+    teamLeadId: string
+  ): Observable<Rms_Interview> {
+    return this.http.post<Rms_Interview>(
+      `${this.baseurl}/scheduleInterview/${teamLeadId}`,
+      interview
+    );
   }
   getAllEmployeeInterviewDetails(): Observable<EmployeeInterviewDetailsDto[]> {
-    return this.http.get<EmployeeInterviewDetailsDto[]>(`${this.baseurl}/getAllEmployeeInterviewDetails`);
+    return this.http.get<EmployeeInterviewDetailsDto[]>(
+      `${this.baseurl}/getAllEmployeeInterviewDetails`
+    );
   }
 
-  updateInterviewStatus(interviewId: number, status: string): Observable<Rms_Interview> {
+  updateInterviewStatus(
+    interviewId: number,
+    status: string
+  ): Observable<Rms_Interview> {
     const url = `${this.baseurl}/updateInterviewStatus/${interviewId}/${status}`; // Use correct path variables
 
     return this.http.put<Rms_Interview>(url, null, {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+        'Content-Type': 'application/json',
+      }),
     });
   }
 }
-
-
-
-
-
