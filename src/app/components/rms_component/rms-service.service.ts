1090f7f93df7392ef471ab5444f8795500658c27
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-10-22 14:50:00 +0530

    Aman (#117)
    * 06-09-2024
    
    * 09-09-2024
    
    * 09-09-2024
    
    * 11-09-2024
    
    * 11-09-2024
    
    * 12-09-2024
    
    * 13-09-2024
    
    * 13-09-2023
    
    * 17-09-2024
    
    * 17-02-2024
    
    * 19-09-2024
    
    * 23-09-2024
    
    * 24-09-2024
    
    * 26-09-2024
    
    * 26-09-2024
    
    * 30-09-2024
    
    * 30-09-2024
    
    * 01-10-2024
    
    * 01-10-2024
    
    * 08-10-2024
    
    * 08-10-2001
    
    * 08-10-2024
    
    * 08-10-2001
    
    * 08-10-2024
    
    * 08-10-2024
    
    * 08-10-2024
    
    * 08-10-2024
    
    * 456
    
    * 87465
    
    * 09-10-2024
    
    * 09-10-2024
    
    * 14-10-2024
    
    * 14-10-2024
    
    * 14-10-2024
    
    * 15-10-2024
    
    * 15-10-2024
    
    * 15-10-2024
    
    * 22-10-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    Co-authored-by: w3softechindia <160119484+w3softechindia@users.noreply.github.com>

--- src/app/components/rms_component/rms-service.service.ts
+++ src/app/components/rms_component/rms-service.service.ts
@@ -11,7 +11,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class RmsServiceService {
   // private baseurl = 'http://localhost:8082';
 
-  private baseurl = 'https://lms-backend-5e890b1bbe26.herokuapp.com';
+  private baseurl = 'https:///lms-backend-5e890b1bbe26.herokuapp.com';
 
   constructor(private http: HttpClient) {}
 
