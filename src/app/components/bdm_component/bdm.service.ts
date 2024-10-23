d923f36cc923af4c6a857ec290bbcd0ab6ad3109
Sachin Putta <33392966+sachinputta@users.noreply.github.com>
2024-10-23 13:32:00 +0530

    Sachin (#122)
    * 10:35 (#15)
    
    * 06-09-2024 (#4)
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06/09/2024 (#5)
    
    * Chidwi (#6)
    
    * 06-09-2024
    
    * 06-09-2024
    
    ---------
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06-09-2024 (#7)
    
    * Nani (#8)
    
    * 06/09/2024
    
    * Nani-06/09/2024
    
    * Chidwi (#9)
    
    * 06-09-2024
    
    * 06-09-2024
    
    * 06-09-2024
    
    ---------
    
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    
    * 06-09-2024 (#10)
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Nani (#11)
    
    * 06/09/2024
    
    * Nani-06/09/2024
    
    * 06/09/2024
    
    * Aman (#12)
    
    * 06-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Aman (#13)
    
    * 06-09-2024
    
    * 09-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Amanmulla6499 <amanmulla6499>
    
    * Vinitha (#14)
    
    * 06-09-2024
    
    * 09-09-2024
    
    ---------
    
    Co-authored-by: Gangineni Chinmay Chidwi <126610157+ChinmayChidwi@users.noreply.github.com>
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    Co-authored-by: Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
    Co-authored-by: vinitha111 <161420839+vinitha111@users.noreply.github.com>
    Co-authored-by: Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
    
    * 11-09-2024
    
    * 11-09-2024
    
    * 13-09-2024
    
    * sachin
    
    * 17-09-2024
    
    * 26-09-2024
    
    * 30-09-2024
    
    * 03-01-2024
    
    * 23-10-2024
    
    * 23-10-2024
    
    ---------
    
    Co-authored-by: Gangineni Chinmay Chidwi <126610157+ChinmayChidwi@users.noreply.github.com>
    Co-authored-by: Sri-Charan-0516 <sricharan824@gmail.com>
    Co-authored-by: Sri-Charan-0516 <147786405+Sri-Charan-0516@users.noreply.github.com>
    Co-authored-by: vinitha111 <161420839+vinitha111@users.noreply.github.com>
    Co-authored-by: Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>

--- src/app/components/bdm_component/bdm.service.ts
+++ src/app/components/bdm_component/bdm.service.ts
@@ -12,7 +12,9 @@ import { Employee } from 'src/app/Models/Employee';
   providedIn: 'root',
 })
 export class BdmService {
+
   constructor(private http: HttpClient, private auth: AuthService) {}
+
 
   // private baseurl = 'http://localhost:8082';
   private baseurl = 'https://lms-backend-5e890b1bbe26.herokuapp.com';
@@ -21,6 +23,7 @@ export class BdmService {
 
   // CREATE
   createItem(data: any): Observable<any> {
+
     return this.http.post(`${this.baseurl}/createClient`, data);
   }
 
@@ -40,8 +43,12 @@ export class BdmService {
 
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-      console.error(`${operation} failed: ${error.message}`); // Log error to console
+
+
+      console.error(`${operation} failed: ${error.message}`);  // Log error to console
       return of(result as T);
+
+
     };
   }
 
@@ -77,6 +84,7 @@ export class BdmService {
   ): Observable<BdmClient> {
     const url = `${this.baseurl}/resetClientPassword/${companyId}/${currentPassword}/${newPassword}`;
     return this.http.put<BdmClient>(url, null); // No body needed, just pass null
+
   }
   getGoodEmployees(): Observable<Deployment[]> {
     return this.http.get<Deployment[]>(`${this.baseurl}/getGoodEmployees`);
@@ -86,9 +94,11 @@ export class BdmService {
     return this.http.get<Deployment[]>(`${this.baseurl}/getAverageEmployees`);
   }
 
+
   getPoorEmployees(): Observable<Deployment[]> {
     return this.http.get<Deployment[]>(`${this.baseurl}/getPoorEmployees`);
   }
+
 
   // Fetch employees based on role and status
   getEmployeesByRoleAndStatus(role: string, status: string): Observable<any[]> {
@@ -104,10 +114,22 @@ export class BdmService {
     return this.http.get(`${this.baseurl}/getAllClient`);
   }
 
+
   addEmployeeToClient(companyId: number, employeeId: string): Observable<any> {
     return this.http.post(
       `${this.baseurl}/addEmployeeToBdmClient/${companyId}/${employeeId}`,
       {}
     );
   }
+
+
+
+  // get Employess to fetch in BDM Deployment Details
+
+  getEmployees(): Observable<any> {
+    return this.http.get(`${this.baseurl}/getAllEmployeesInBdm`);
+  }
+
+
 }
+
