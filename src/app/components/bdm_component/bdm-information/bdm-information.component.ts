abf5c9f533557ddd2518d99d9af819a6b19a8457
sachinputta <puttasachin6@gmail.com>
2024-10-23 13:30:19 +0530

    23-10-2024

--- src/app/components/bdm_component/bdm-information/bdm-information.component.ts
+++ src/app/components/bdm_component/bdm-information/bdm-information.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BdmService } from '../bdm.service';
 
 @Component({
   selector: 'app-bdm-information',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./bdm-information.component.scss']
 })
 export class BdmInformationComponent {
+
+  employees: any[] = [];
+
+  constructor(private bdmService: BdmService,) {}
+
+  ngOnInit(): void {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees(): void {
+    this.bdmService.getEmployees().subscribe(
+      (data) => {
+        // Map relevant fields here
+        this.employees = data.map((employee: any) => ({
+          employeeId: employee.employeeId,
+          fullName: `${employee.firstName} ${employee.lastName}`,
+          roles: employee.roles.map((role: any) => role.roleName), 
+          dateOfJoin: employee.dateOfJoin || 'N/A',
+          status: employee.status || 'N/A'
+        }));
+
+      },
+      (error) => {
+        console.error('Error fetching employees:', error);
+      }
+    );
+  }
 
 }
