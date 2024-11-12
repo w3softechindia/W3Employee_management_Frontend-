7f87efd2c6a9d922b1b793d3764c6b24b267b158
Shaikkaif <61644820+Shaikkaif@users.noreply.github.com>
2024-11-12 17:52:30 +0530

    Kaif (#150)
    * 06/11/2024
    
    * 12/11/2024-kaif
    
    ---------
    
    Co-authored-by: w3softechindia <160119484+w3softechindia@users.noreply.github.com>

--- src/app/components/rms_component/rms-service.service.ts
+++ src/app/components/rms_component/rms-service.service.ts
@@ -11,9 +11,9 @@ import { Applicant } from 'src/app/Models/applicant';
   providedIn: 'root',
 })
 export class RmsServiceService {
-  // private baseurl = 'http://localhost:8082';
+   private baseurl = 'http://localhost:8082';
 
-  private baseurl = 'https:///lms-backend-5e890b1bbe26.herokuapp.com';
+ // private baseurl = 'https:///lms-backend-5e890b1bbe26.herokuapp.com';
 
   constructor(private http: HttpClient) {}
 
