98a16a018d0117d248c3b55d7f3596c0c697bf2a
Moin <85317674+MoinDotCalm@users.noreply.github.com>
2024-11-12 17:30:43 +0530

    12-11-2024--moin (#149)

--- server.js
+++ server.js
@@ -7,4 +7,5 @@ app.use(express.static(__dirname+'/dist/edon-ng'));
 app.get('/*',(req,resp)=>{
     resp.sendFile(__dirname+'/dist/edon-ng/index.html');
 });
+
 app.listen(process.env.PORT || 4200);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -121,7 +121,7 @@ import { TeamleadAttendanceTrackComponent } from './components/instructor-dashbo
 import { RmsInterviewComponent } from './components/rms_component/rms-interview/rms-interview.component';
 
 import { BdmNavbarComponent } from './components/bdm_component/bdm-navbar/bdm-navbar.component';
-import { AddTeamComponent } from './components/admin-dashboard/instructor-add-team/add-team.component';
+// import { AddTeamComponent } from './components/admin-dashboard/instructor-add-team/add-team.component';
 import { BdmClientComponent } from './components/bdm_component/bdm-client/bdm-client.component';
 import { BdmSettingComponent } from './components/bdm_component/bdm-setting/bdm-setting.component';
 import { RmsNavbarComponent } from './components/rms_component/rms-navbar/rms-navbar.component';
