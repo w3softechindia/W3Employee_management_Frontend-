471eb60c14c46c79091f05bc74209d0befc11195
purna11111 <purnapuri14@gmal.com>
2024-11-14 12:41:52 +0530

    11-12-2024

---
+++ src/app/components/rms_component/pay-slips/pay-slips.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaySlipsComponent } from './pay-slips.component';
+
+describe('PaySlipsComponent', () => {
+  let component: PaySlipsComponent;
+  let fixture: ComponentFixture<PaySlipsComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PaySlipsComponent]
+    });
+    fixture = TestBed.createComponent(PaySlipsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
